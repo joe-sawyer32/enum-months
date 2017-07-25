85fd0d0df96534441b739b4f005e271d3eac95e3
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-25 17:55:59 -0500

    Light refactoring, input request looped on valid input failure

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -9,36 +9,51 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        outputOptions();
+        Month userMonth = null;
+        boolean isValidInput = false;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("------------------------");
+            System.out.print("Please choose a month (All Caps Please): ");
+            try {
+                userMonth = Month.valueOf(scanner.nextLine());
+                isValidInput = true;
+            } catch (IllegalArgumentException ex) {
+                System.out.println("\nThis is not a valid month (or is not in the correct format).");
+                System.out.println("Error Message: " + ex);
+            }
+        } while (!isValidInput);
+        System.out.printf("\nSelected month: %s\n", userMonth.getEnglishName());
+        List<String> userMonthAndBeyond = new ArrayList<>();
+        userMonthAndBeyond = createMonthList(userMonth, userMonthAndBeyond);
+        System.out.println("Here's a look at the rest of the year:");
+        System.out.println(userMonthAndBeyond);
+
+    }
+
+    private static void outputOptions() {
         for (Month m : Month.values()) {
             System.out.println(m);
         }
-        System.out.println("------------------------");
-        System.out.print("Please choose a month (All Caps Please): ");
-        try (Scanner scanner = new Scanner(System.in)) {
-            Month userMonth = Month.valueOf(scanner.nextLine());
-            System.out.printf("\nSelected month: %s\n", userMonth.getEnglishName());
-            List<String> userMonthAndBeyond= new ArrayList<>();
-            switch (userMonth) {
-                case JANUARY: userMonthAndBeyond.add(Month.JANUARY.getEnglishName());
-                case FEBRUARY: userMonthAndBeyond.add(Month.FEBRUARY.getEnglishName());
-                case MARCH: userMonthAndBeyond.add(Month.MARCH.getEnglishName());
-                case APRIL: userMonthAndBeyond.add(Month.APRIL.getEnglishName());
-                case MAY: userMonthAndBeyond.add(Month.MAY.getEnglishName());
-                case JUNE: userMonthAndBeyond.add(Month.JUNE.getEnglishName());
-                case JULY: userMonthAndBeyond.add(Month.JULY.getEnglishName());
-                case AUGUST: userMonthAndBeyond.add(Month.AUGUST.getEnglishName());
-                case SEPTEMBER: userMonthAndBeyond.add(Month.SEPTEMBER.getEnglishName());
-                case OCTOBER: userMonthAndBeyond.add(Month.OCTOBER.getEnglishName());
-                case NOVEMBER: userMonthAndBeyond.add(Month.NOVEMBER.getEnglishName());
-                case DECEMBER: userMonthAndBeyond.add(Month.DECEMBER.getEnglishName());
-                break;
-            }
-            System.out.println("Here's a look at the rest of the year:");
-            System.out.println(userMonthAndBeyond);
-        } catch (IllegalArgumentException ex) {
-            System.out.println("\nThis is not a valid month (or is not in the correct format).");
-            System.out.println(ex);
-        }
+    }
 
+    private static List<String> createMonthList(Month month, List<String> list) {
+        switch (month) {
+            case JANUARY: list.add(Month.JANUARY.getEnglishName());
+            case FEBRUARY: list.add(Month.FEBRUARY.getEnglishName());
+            case MARCH: list.add(Month.MARCH.getEnglishName());
+            case APRIL: list.add(Month.APRIL.getEnglishName());
+            case MAY: list.add(Month.MAY.getEnglishName());
+            case JUNE: list.add(Month.JUNE.getEnglishName());
+            case JULY: list.add(Month.JULY.getEnglishName());
+            case AUGUST: list.add(Month.AUGUST.getEnglishName());
+            case SEPTEMBER: list.add(Month.SEPTEMBER.getEnglishName());
+            case OCTOBER: list.add(Month.OCTOBER.getEnglishName());
+            case NOVEMBER: list.add(Month.NOVEMBER.getEnglishName());
+            case DECEMBER: list.add(Month.DECEMBER.getEnglishName());
+                break;
+        }
+        return list;
     }
 }
